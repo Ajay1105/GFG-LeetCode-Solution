dd036d857af9b9d88d5a85fd944c74f1f4f5851a
Ajay Kumar Yadav <96533388+Ajay1105@users.noreply.github.com>
2023-08-27 10:58:52 +0530

    Time: 63 ms (88.26%), Space: 23.3 MB (84.86%) - LeetHub

---
+++ 0403-frog-jump/0403-frog-jump.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    bool util(vector<int> &stones, unordered_map<int, unordered_map<int, bool>> &mp, int pos, int units) {
+    if(pos == stones.back()) return true;
+    if(mp.find(pos) != mp.end() && mp[pos].find(units) != mp[pos].end()) return mp[pos][units];
+
+    for(int i = units-1; i <= units+1; i++){
+        if(i>0){
+            int next_pos = pos + i;
+            if(binary_search(stones.begin(), stones.end(), next_pos) && util(stones, mp, next_pos, i)){
+                mp[pos][units] = true;
+                return true;
+            }
+        }
+    }
+    return mp[pos][units] = false;       
+    }
+
+    bool canCross(vector<int>& stones) {
+        unordered_map<int, unordered_map<int, bool>> mp;
+    if (stones[1] != 1) return false;
+    return util(stones, mp, 1, 1);
+    }
+};
