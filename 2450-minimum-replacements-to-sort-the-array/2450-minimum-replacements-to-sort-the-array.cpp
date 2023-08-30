865f3aeefc97d4c0f5ea5b021479df0078ddc44f
Ajay Kumar Yadav <96533388+Ajay1105@users.noreply.github.com>
2023-08-30 10:35:00 +0530

    Time: 43 ms (99.7%), Space: 53.9 MB (100%) - LeetHub

--- 2450-minimum-replacements-to-sort-the-array/2450-minimum-replacements-to-sort-the-array.cpp
+++ 2450-minimum-replacements-to-sort-the-array/2450-minimum-replacements-to-sort-the-array.cpp
@@ -1,4 +1,4 @@
-int fastio = [] {std::ios::sync_with_stdio(0); std::cin.tie(0); return 0; }();
+int fastio = [] {std::ios::sync_with_stdio(0); std::cin.tie(0); return 0; }(); // make code run faster
 class Solution {
 public:
     long long minimumReplacement(vector<int>& nums) {
@@ -7,9 +7,14 @@ public:
 
         for(auto it = nums.rbegin()+1; it != nums.rend(); it++){
             long long num = *it;
+
+            if(num >= upper_bound){
             long long no_of_times = (num + upper_bound -1)/upper_bound;
             ans += no_of_times-1;
             upper_bound = num/no_of_times;
+            }else{
+                upper_bound = num;
+            }
         }
 
         return ans;
