9c703b06aa0e3d4399366dc9780b55c57708c45c
Ajay Kumar Yadav <96533388+Ajay1105@users.noreply.github.com>
2023-09-28 11:25:59 +0530

    Time: 11 ms (24.58%), Space: 16.4 MB (36.74%) - LeetHub

--- 0941-sort-array-by-parity/0941-sort-array-by-parity.cpp
+++ 0941-sort-array-by-parity/0941-sort-array-by-parity.cpp
@@ -1,18 +1,9 @@
 class Solution {
 public:
-    vector<int> sortArrayByParity(vector<int>& v) {
-         int i = 0, j = v.size() - 1, len = v.size();
-        if (len < 2) return v;
-        while (true) {
-            // moving to the first odd number with i
-            while (i < len && !(v[i] % 2)) i++;
-            // moving to the first even number with j
-            while (j && v[j] % 2) j--;
-			// checking if we are still in a valid scenario, otherwise we exit
-            if (i >= j) break;
-			// if so, we swap and update i and j
-            swap(v[i++], v[j--]);
-        }
-        return v;
+    vector<int> sortArrayByParity(vector<int>& nums) {
+        sort(nums.begin(), nums.end(), [](const int &a, const int &b) {
+            return (a % 2) < (b % 2);
+        });
+        return move(nums);
     }
 };
